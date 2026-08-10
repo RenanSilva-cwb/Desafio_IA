5ab517d6068791d303a8d7e11adb0cabacd7cdac
Leonardo <leonardosilvalima40@gmail.com>
2026-08-10 16:50:05 -0300

    API Render

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = '/api';
+const BASE_URL = 'https://backend-academia.onrender.com/api';
 
 async function request(path, options = {}) {
   const response = await fetch(`${BASE_URL}${path}`, {
